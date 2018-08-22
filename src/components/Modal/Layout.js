e1b043758893502cada5fc799f6616b8196f12a7
elishakay <kramer1346@gmail.com>
2018-08-22 23:14:02 +0300

    added default image for new book

--- src/components/Modal/Layout.js
+++ src/components/Modal/Layout.js
@@ -1,27 +1,14 @@
 import React from 'react'
-import Modal from './Modal'
-import ImageHead from './ImageHead'
-import ImageContent from './ImageContent'
+import AddBook from '../Books/AddBook'
+
+import newbook from '../Books/newbook'
 
 export default ({ children }) => (
   <React.Fragment>
     <div className="title">
       <h1>ReactJS Book Library</h1>
     </div>
-    <div className='grid__item'>
-	    <Modal
-	      maxwidth={700}
-	      maxheight={500}
-	      ms={500}
-	    >
-	      <Modal.Head>
-	        <ImageHead />
-	      </Modal.Head>
-	      <Modal.Content>
-	        <ImageContent />
-	      </Modal.Content>
-	    </Modal>
-	</div>
+    <AddBook {...newbook}/>
 
     {children}
     <div className="credits">Created with <span className="love"></span> by <a href="https://github.com/ElishaKay?tab=repositories">Elisha Kramer</a></div>
