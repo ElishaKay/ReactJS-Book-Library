a1c8f652322184a5f74b01622594c338fc0eb6cb
elishakay <kramer1346@gmail.com>
2018-08-23 08:31:13 +0300

    update book to savebook

--- src/reducers/books-reducer.js
+++ src/reducers/books-reducer.js
@@ -1,11 +1,11 @@
 import _ from "lodash";
-import { FETCH_BOOKS, UPDATE_BOOK, DELETE_BOOK } from "../actions/types";
+import { FETCH_BOOKS, SAVE_BOOK, DELETE_BOOK } from "../actions/types";
 
 export default function(state = {}, action) {
   switch (action.type) {
     case DELETE_BOOK:
       return _.omit(state, action.payload);
-    case UPDATE_BOOK:
+    case SAVE_BOOK:
       return { ...state, [action.payload.id]: action.payload };
     case FETCH_BOOKS:
       console.log('action.payload: ',action.payload)
