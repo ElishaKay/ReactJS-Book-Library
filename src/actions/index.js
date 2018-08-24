a5ffc5d2d0f9ef233cc9810729c3d403bf6485c6
elishakay <kramer1346@gmail.com>
2018-08-24 13:34:48 +0300

    Titles always capitalized - even when saved

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,17 @@ import { FETCH_BOOKS, SAVE_BOOK, DELETE_BOOK } from './types';
 
 const ROOT_URL = "https://www.googleapis.com/books/v1/volumes?q=computers";
 
+function filterTitle(str) {
+     let newTitle = '';
+     let splitStr = str.toLowerCase().split(' ');
+     for (let i = 0; i < splitStr.length; i++) {
+       let newWord = splitStr[i].replace(/\W/g, '')
+       newWord = newWord.charAt(0).toUpperCase() + newWord.substr(1);
+       newTitle += newWord + ' ';   
+     }
+     return newTitle.trim()
+}
+
 export const fetchBooks = () => async dispatch => {
   const res = await axios.get(`${ROOT_URL}`);
 
@@ -13,7 +24,7 @@ export const fetchBooks = () => async dispatch => {
     let book = res.data.items[i].volumeInfo;
     let imgSrc = book.imageLinks ? book.imageLinks.thumbnail.replace("zoom=1", "zoom=100") : 'http://vollrath.com/ClientCss/images/VollrathImages/No_Image_Available.jpg'
     books[i] = {id: id++,
-                title: book.title,
+                title: filterTitle(book.title),
                 author: book.authors[0], 
                 img: imgSrc,
                 published: book.publishedDate,
@@ -25,6 +36,8 @@ export const fetchBooks = () => async dispatch => {
 
 export const saveBook = (values, callback) => dispatch => {
   callback();
+  console.log(values);
+  values.title = filterTitle(values.title);
   dispatch({ type: SAVE_BOOK, payload: values });
 }
 
