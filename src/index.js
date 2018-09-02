ffcda69baf6e827ffa704f120efe9223e09798a8
Elisha Kramer <Kramer1346@gmail.com>
2018-09-02 12:46:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,8 +21,8 @@ if (process.env.NODE_ENV === 'production') {
   store = createStore(reducers,
 		{},
 		compose(
-	    applyMiddleware(...middleware)
-	    , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	    applyMiddleware(...middleware),
+	    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	  )
 	);
 }
